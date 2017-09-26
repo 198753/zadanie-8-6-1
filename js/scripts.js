a338848b463b6c2457de8c7031152d5c4d962fa8
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-09-26 08:38:12 +0200

    Wyszło

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+var a = 70,
+	b = 10,
+	value = (a * a) + (2 * a * b) - (b * b);
+	
+console.log('wynik = ' + value);
+
+if (value != 0) {
+console.log((value > 0) ? 'wartość dodatnia' : 'wartośc ujemna');
+} else {
+	alert('wynik jest równy zero');
+}
+	
+
+
+
+
